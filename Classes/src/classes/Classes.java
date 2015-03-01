f70302356070fe2e290ab10d40f9989627bf0c65
Gareth Burke <shakesgar@gmail.com>
2015-03-01 01:13:02 +0000

    Classes test

---
+++ Classes/src/classes/Classes.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package classes;
+
+/**
+ *
+ * @author Protools
+ */
+public class Classes {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       Welcome welcome = new Welcome();
+       welcome.sayHello();
+    }
+    
+}
