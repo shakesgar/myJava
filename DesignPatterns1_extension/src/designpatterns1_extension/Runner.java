0c6795b7ad018546882cd18a9b82c271eb92a56c
Gareth Burke <shakesgar@gmail.com>
2015-03-01 20:06:07 +0000

    Another test

--- DesignPatterns1_extension/src/designpatterns1_extension/Runner.java
+++ DesignPatterns1_extension/src/designpatterns1_extension/Runner.java
@@ -7,6 +7,6 @@ package designpatterns1_extension;
 class Runner{
     public static void main(String[] args){
 	String  s = new String("text");
-	System.out.println(s);				//Prints "text" not a 'String@e231ce21'... why?
+	System.out.println(s+ " ");				//Prints "text" not a 'String@e231ce21'... why?
 	}
 }
