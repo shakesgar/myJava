f05920e6a7240dfe4dbc39567621e887ab9afd7c
Gareth Burke <shakesgar@gmail.com>
2015-03-01 20:08:29 +0000

    DEsign Patterns 1

---
+++ DesignPatterns_BeginnerCode1/src/Person.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Protools
+ */
+public class Person {
+    public String name;				//one attribute
+	
+	public Person(String aName){	//one constructor
+		this.name = aName;
+	}
+}
---
+++ DesignPatterns_BeginnerCode1/src/Runner.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Protools
+ */
+class Runner{
+	public static void main(String[] args){
+		Person p = new Person("Rob");
+		System.out.println(p.name);
+		//System.out.println(p);		//What gets printed?
+		
+	}
+}
