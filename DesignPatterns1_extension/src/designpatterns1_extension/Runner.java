dc25e212f94a6eded6ea598bc169643b765d0952
Gareth Burke <shakesgar@gmail.com>
2015-03-01 20:03:52 +0000

    test

---
+++ DesignPatterns1_extension/src/designpatterns1_extension/Runner.java
@@ -0,0 +1,12 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package designpatterns1_extension;
+class Runner{
+    public static void main(String[] args){
+	String  s = new String("text");
+	System.out.println(s);				//Prints "text" not a 'String@e231ce21'... why?
+	}
+}
